af8ac234dc05a1e025018f0e08fb67aa70b8b4d5
autchadaporn <inging.510801@gmail.com>
2020-04-13 09:18:23 +0700

    first commit

---
+++ connect.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,request,redirect,url_for
+import pymysql
+
+# Start connect DB
+app = Flask(__name__)
+#conn=pymysql.connect('localhost','root','','studentdb')
+# Stop connect
+
+@app.route("/")
+def login():
+    return render_template('login.html')
+app.run(debug=True)   
