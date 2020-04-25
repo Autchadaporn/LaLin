d9a1889f959a136e67795bfd612eb90f577882fe
autchadaporn <inging.510801@gmail.com>
2020-04-25 21:07:33 +0700

    search

--- connect.py
+++ connect.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template,request,redirect,url_for
-import pymysql
+from flask import Flask, render_template, redirect, request, url_for, session,flash
+from flask_mysqldb import MySQL, MySQLdb
+from sqlalchemy import text
+import bcrypt
 
 # Start connect DB
 app = Flask(__name__)
-#conn=pymysql.connect('localhost','root','','studentdb')
+
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_DB']='calendar'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
 # Stop connect
+mysql = MySQL(app)
 
 @app.route("/login")
 def login():
@@ -12,6 +20,31 @@ def login():
 
 @app.route("/test")
 def test():
-    return render_template('test.html')   
+    return render_template('test.html') 
 
+@app.route("/calendar")
+def calendar(): 
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM calendar")
+        # cur.execute("SELECT * FROM topic")
+        rows=cur.fetchall()
+        return render_template('calendar.html',datas=rows)
+
+@app.route("/formsearch")
+def formsearch():
+    return render_template('formsearch.html')
+
+
+@app.route("/search" ,methods=['GET', 'POST'])
+def search():
+    topic =  request.form['Topic']
+    term =  request.form['Term']
+    # print(topic)
+    # print(term)
+    cur = mysql.connection.cursor() #เชื่อมdatabase
+    cur.execute("SELECT Start_Date,Finish_Date FROM calendar WHERE Topic_ID = '"+topic+"' AND Term = '"+term+"'")
+    rows=cur.fetchall()
+    return render_template('search.html',datas = rows)
+    
+    
 app.run(debug=True)
