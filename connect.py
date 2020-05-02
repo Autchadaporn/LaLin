9cabd4b03177798240e4c76414a6fca50cb13500
autchadaporn <inging.510801@gmail.com>
2020-05-02 13:14:43 +0700

    update

--- connect.py
+++ connect.py
@@ -25,7 +25,7 @@ def test():
 @app.route("/calendar")
 def calendar(): 
         cur = mysql.connection.cursor()
-        cur.execute("SELECT topic.Topic, calendar.Term, calendar.Start_Date, calendar.Finish_Date, calendar.Year FROM topic INNER JOIN calendar ON topic.Topic_ID = calendar.Topic_ID  ")
+        cur.execute("SELECT topic.Topic_ID, topic.Topic, calendar.Term, calendar.Start_Date, calendar.Finish_Date, calendar.Year FROM topic INNER JOIN calendar ON topic.Topic_ID = calendar.Topic_ID  ")
         rows=cur.fetchall()
         return render_template('calendar.html',datas=rows)
 
@@ -44,6 +44,39 @@ def search():
     cur.execute("SELECT Start_Date,Finish_Date FROM calendar WHERE Topic_ID = '"+topic+"' AND Term = '"+term+"'")
     rows=cur.fetchall()
     return render_template('search.html',datas = rows)
+
+@app.route("/addcalendar", methods=['POST','GET'])
+def addcalendar():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM topic")
+    rows=cur.fetchall()
+    Topicid = request.form['Topic']
+    SDate = request.form['SDate']
+    FDate = request.form['FDate']
+    Term = request.form['Term']
+    Year = request.form['Year']
+    # print(Topicid,SDate,FDate,Term,Year)
+    cur.execute("INSERT INTO calendar (Topic_ID,Term,Start_Date,Finish_Date,Year) VALUES (%s,%s,%s,%s,%s)",(Topicid,Term,SDate,FDate,Year))
+    mysql.connection.commit()
+    return render_template('addcalendar.html',datas=rows)
+
+@app.route("/addtopic")
+def addtopic():
+    return render_template('addtopic.html')
+
+@app.route("/inserttopic",methods=["POST"])
+def insert():
+    topicid = request.form['TopicID']
+    topic = request.form['Topic']
+    # print(topicid,topic)
+    cur = mysql.connection.cursor()
+    cur.execute (" INSERT INTO topic (Topic_ID,Topic) VALUES (%s,%s)",(topicid,topic))
+    # print(cur)
+    # cur.execute(cur)
+    mysql.connection.commit()
+    return render_template('addtopic.html')
+
+
     
     
 app.run(debug=True)
